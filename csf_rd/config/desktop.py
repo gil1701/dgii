9484f1452c6953b8dd48058b78f64bd6d9c6bca4
ODALIS GUTIERREZ <gil1701@hotmail.com>
2026-05-24 15:35:03 -0400

    Update desktop.py

--- csf_rd/config/desktop.py
+++ csf_rd/config/desktop.py
@@ -1,1 +1,11 @@
 # CSF RD Desktop Configuration
+from frappe import _
+
+def get_data():
+    return [
+        {
+            "module_name": "CSF RD",
+            "type": "module",
+            "label": _("CSF RD")
+        }
+    ]
